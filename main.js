78a5775d10aafd9cd01cc7479750763a14fff880
ImCrimP <pjmountin@me.com>
2023-08-18 09:07:53 -0500

    created styling for library: book cards and grid

--- main.js
+++ main.js
@@ -1,21 +1,15 @@
 const addBook = document.querySelector("#addBtn");
 
-
 const myLibrary = [];
 
-
-addBook.addEventListener("click", ()=> {
-    addBookToLibrary();
+addBook.addEventListener("click", () => {
+  addBookToLibrary();
 });
 
-
-
-function book(title, author, read){
-    this.title = title;
-    this.author = author;
-    this.read = read;
+function book(title, author, read) {
+  this.title = title;
+  this.author = author;
+  this.read = read;
 }
 
-function addBookToLibrary(){
-    
-}
+function addBookToLibrary() {}
