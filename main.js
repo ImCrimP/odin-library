13ae22deebfdb9c8aca8bdb495e5f73515ed47e2
ImCrimP <pjmountin@me.com>
2023-08-18 07:25:56 -0500

    added header and add book button

---
+++ main.js
@@ -0,0 +1,21 @@
+const addBook = document.querySelector("#addBtn");
+
+
+const myLibrary = [];
+
+
+addBook.addEventListener("click", ()=> {
+    addBookToLibrary();
+});
+
+
+
+function book(title, author, read){
+    this.title = title;
+    this.author = author;
+    this.read = read;
+}
+
+function addBookToLibrary(){
+    
+}
