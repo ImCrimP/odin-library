2810e48e474bf834f654d4f75b4530ace33a9f4b
ImCrimP <pjmountin@me.com>
2023-08-18 10:29:55 -0500

    add book makes form visible and submit makes form hidden

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const addBook = document.querySelector("#addBtn");
+const form = document.querySelector("#form");
+const submit = document.querySelector("#submitInput");
 
 const myLibrary = [];
 
@@ -12,4 +14,21 @@ function book(title, author, read) {
   this.read = read;
 }
 
-function addBookToLibrary() {}
+function addBookToLibrary() {
+  form.style.visibility = "visible";
+  const book = document.createElement("div");
+  const title = document.createElement("div");
+  const author = document.createElement("div");
+  const read = document.createElement("button");
+  const remove = document.createElement("button");
+
+  book.setAttribute("id", "book");
+  title.setAttribute("id", "title");
+  author.setAttribute("id", "author");
+  read.setAttribute("id", "read");
+  remove.setAttribute("id", "remove");
+
+  submit.addEventListener("click", () => {
+    form.style.visibility = "hidden";
+  });
+}
